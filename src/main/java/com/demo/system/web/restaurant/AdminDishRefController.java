3db42cd083d5a1cf29451b55cc2ee747ce3a7fc4
genakimov <ga.spb7@gmail.com>
2022-03-07 20:20:56 +0300

    Implement dish ref manage

---
+++ src/main/java/com/demo/system/web/restaurant/AdminDishRefController.java
@@ -0,0 +1,91 @@
+package com.demo.system.web.restaurant;
+
+import com.demo.system.model.DishRef;
+import com.demo.system.repository.DishRefRepository;
+import com.demo.system.service.DishRefService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.util.List;
+
+import static com.demo.system.util.validation.ValidationUtil.assureIdConsistent;
+import static com.demo.system.util.validation.ValidationUtil.checkNew;
+
+@RestController
+@RequestMapping(value = AdminDishRefController.REST_URL, produces = MediaType.APPLICATION_JSON_VALUE)
+@AllArgsConstructor
+@Slf4j
+public class AdminDishRefController {
+
+    static final String REST_URL = "/api/admin/restaurants/{restaurantId}/dish-ref";
+
+    private final DishRefRepository repository;
+    private final DishRefService service;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<DishRef> get(@PathVariable int restaurantId, @PathVariable int id) {
+        log.info("get for restaurantId={}, id={}", restaurantId, id);
+        return ResponseEntity.of(repository.get(restaurantId, id));
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable int restaurantId, @PathVariable int id) {
+        log.info("delete for restaurantId={}, id={}", restaurantId, id);
+        DishRef dishRef = repository.checkBelong(restaurantId, id);
+        repository.delete(dishRef);
+    }
+
+    @GetMapping
+    public List<DishRef> getByRestaurant(@PathVariable int restaurantId) {
+        log.info("getByRestaurant for restaurantId={}", restaurantId);
+        return repository.getByRestaurant(restaurantId);
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<DishRef> createWithLocation(@PathVariable int restaurantId, @Valid @RequestBody DishRef dishRef) {
+        log.info("create {} for restaurantId={}", dishRef, restaurantId);
+        checkNew(dishRef);
+        DishRef created = service.save(restaurantId, dishRef);
+        URI uriOfNewResource = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path(REST_URL + "/{id}")
+                .buildAndExpand(restaurantId, created.getId()).toUri();
+        return ResponseEntity.created(uriOfNewResource).body(created);
+    }
+
+    @PutMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void update(@PathVariable int restaurantId, @PathVariable int id, @Valid @RequestBody DishRef dishRef) {
+        log.info("update {} for restaurantId={}, id={}", dishRef, restaurantId, id);
+        assureIdConsistent(dishRef, id);
+        repository.checkBelong(restaurantId, id);
+        service.save(restaurantId, dishRef);
+    }
+
+    @PatchMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @Transactional
+    public void enable(@PathVariable int restaurantId, @PathVariable int id, @RequestParam boolean enabled) {
+        log.info(enabled ? "enable {}" : "disable {}", id);
+        DishRef dishRef = repository.checkBelong(restaurantId, id);
+        dishRef.setEnabled(enabled);
+    }
+}
---
+++ src/test/java/com/demo/system/web/restaurant/AdminDishRefControllerTest.java
@@ -0,0 +1,129 @@
+package com.demo.system.web.restaurant;
+
+import com.demo.system.model.DishRef;
+import com.demo.system.repository.DishRefRepository;
+import com.demo.system.util.JsonUtil;
+import com.demo.system.web.AbstractControllerTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static com.demo.system.web.restaurant.RestaurantTestData.DISH_REF_MATCHER;
+import static com.demo.system.web.restaurant.RestaurantTestData.MAC_ID;
+import static com.demo.system.web.restaurant.RestaurantTestData.WASABI_ID;
+import static com.demo.system.web.restaurant.RestaurantTestData.getNewDish;
+import static com.demo.system.web.restaurant.RestaurantTestData.getUpdatedDish;
+import static com.demo.system.web.restaurant.RestaurantTestData.mac_chb;
+import static com.demo.system.web.restaurant.RestaurantTestData.mac_chm20;
+import static com.demo.system.web.restaurant.RestaurantTestData.mac_fof;
+import static com.demo.system.web.restaurant.RestaurantTestData.wasabi_rsh;
+import static com.demo.system.web.user.UserTestData.ADMIN_MAIL;
+import static com.demo.system.web.user.UserTestData.USER_MAIL;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+class AdminDishRefControllerTest extends AbstractControllerTest {
+
+    private static final String REST_URL = AdminRestaurantController.REST_URL + '/';
+
+    @Autowired
+    private DishRefRepository dishRefRepository;
+
+    private String getUrl(int restaurantId) {
+        return REST_URL + restaurantId + "/dish-ref/";
+    }
+
+    private String getUrl(int restaurantId, int id) {
+        return getUrl(restaurantId) + id;
+    }
+
+    @Test
+    @WithUserDetails(value = ADMIN_MAIL)
+    void get() throws Exception {
+        perform(MockMvcRequestBuilders.get(getUrl(MAC_ID, mac_fof.id())))
+                .andExpect(status().isOk())
+                .andDo(print())
+                // https://jira.spring.io/browse/SPR-14472
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(DISH_REF_MATCHER.contentJson(mac_fof));
+    }
+
+    @Test
+    @WithUserDetails(value = USER_MAIL)
+    void getForbidden() throws Exception {
+        perform(MockMvcRequestBuilders.get(getUrl(MAC_ID, mac_fof.id())))
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    @WithUserDetails(value = ADMIN_MAIL)
+    void getNotFound() throws Exception {
+        perform(MockMvcRequestBuilders.get(getUrl(MAC_ID, wasabi_rsh.id())))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    @WithUserDetails(value = ADMIN_MAIL)
+    void delete() throws Exception {
+        perform(MockMvcRequestBuilders.delete(getUrl(WASABI_ID, wasabi_rsh.id())))
+                .andDo(print())
+                .andExpect(status().isNoContent());
+        assertFalse(dishRefRepository.findById(wasabi_rsh.id()).isPresent());
+    }
+
+    @Test
+    @WithUserDetails(value = ADMIN_MAIL)
+    void update() throws Exception {
+        DishRef updated = getUpdatedDish();
+        updated.setId(null);
+        perform(MockMvcRequestBuilders.put(getUrl(MAC_ID, mac_fof.id()))
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(JsonUtil.writeValue(updated)))
+                .andDo(print())
+                .andExpect(status().isNoContent());
+
+        DISH_REF_MATCHER.assertMatch(dishRefRepository.getById(mac_fof.id()), getUpdatedDish());
+    }
+
+    @Test
+    @WithUserDetails(value = ADMIN_MAIL)
+    void createWithLocation() throws Exception {
+        DishRef newDishRef = getNewDish();
+        ResultActions action = perform(MockMvcRequestBuilders.post(getUrl(MAC_ID))
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(JsonUtil.writeValue(newDishRef)))
+                .andExpect(status().isCreated());
+
+        DishRef created = DISH_REF_MATCHER.readFromJson(action);
+        int newId = created.id();
+        newDishRef.setId(newId);
+        DISH_REF_MATCHER.assertMatch(created, newDishRef);
+        DISH_REF_MATCHER.assertMatch(dishRefRepository.getById(newId), newDishRef);
+    }
+
+    @Test
+    @WithUserDetails(value = ADMIN_MAIL)
+    void getByRestaurant() throws Exception {
+        perform(MockMvcRequestBuilders.get(getUrl(MAC_ID)))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(DISH_REF_MATCHER.contentJson(mac_fof, mac_chm20, mac_chb));
+    }
+
+    @Test
+    @WithUserDetails(value = ADMIN_MAIL)
+    void enable() throws Exception {
+        perform(MockMvcRequestBuilders.patch(getUrl(WASABI_ID, wasabi_rsh.id()))
+                .param("enabled", "false")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isNoContent());
+
+        assertFalse(dishRefRepository.getById(wasabi_rsh.id()).isEnabled());
+    }
+}
